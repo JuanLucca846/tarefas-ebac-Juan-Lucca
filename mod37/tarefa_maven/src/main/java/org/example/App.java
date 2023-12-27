9f6fa84619a18931f6db2d18dc874d2ef5064d52
JuanLucca846 <lucca470@gmail.com>
2023-12-27 13:18:22 -0300

    desafio mod37

---
+++ mod37/tarefa_maven/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    final static Logger logger = Logger.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        logger.info( "Hello World!" );
+    }
+}
