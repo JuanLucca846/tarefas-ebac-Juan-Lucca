67dd1cbacf74d2202ee822920679eefe52181d18
JuanLucca846 <lucca470@gmail.com>
2024-08-02 13:46:17 -0300

    desafio mod49

--- mod49/TestesUnitarios/src/main/java/org/example/Main.java
+++ mod49/TestesUnitarios/src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+/*
+ * Sim, porque o código da calculadora não é algo muito complexo, mas conforme o código fica mais complexo
+ * a documentação é algo que ajuda muito, no entendimento de quem for ler e dar manutenção no código
+ * */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
